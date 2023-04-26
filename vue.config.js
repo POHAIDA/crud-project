eadd7012963d6c05722e0cd4dfbeb5d49cee7c62
POHAIDA <114564806+POHAIDA@users.noreply.github.com>
2023-04-26 10:41:44 +0300

    Deploy project to gh-pages

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,10 @@ module.exports = defineConfig({
       },
     },
   },
-})
+});
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/<ваше-имя>.github.io/'
+    : '/'
+}
